b6783a5e4bacf92d05315f79443fe2b99fbdcd90
katyalika <lihachevak05@gmail.com>
2025-03-19 12:47:17 +0300

    classes

---
+++ Cafe/src/main/java/com/mycompany/cafe/Model/modBread.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cafe.Model;
+
+import com.mycompany.cafe.Model.Order;
+
+/**
+ *
+ * @author lihac
+ */
+public class modBread extends Order{
+    
+}
